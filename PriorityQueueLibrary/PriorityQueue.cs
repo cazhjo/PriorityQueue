0ffdb52a551b998d5eb55b2d23b7c8c939eeed96
cazhjo <54894732+cazhjo@users.noreply.github.com>
2019-10-06 17:47:04 +0200

    Changed backingArray to backingList, and changed SwitchPlacesIfNeeded

--- PriorityQueueLibrary/PriorityQueue.cs
+++ PriorityQueueLibrary/PriorityQueue.cs
@@ -1,23 +1,23 @@
 ﻿using PriorityQueue;
 using System;
+using System.Collections.Generic;
 
 namespace PriorityQueueLibrary
 {
     public class PriorityQueue<T> : IPriorityQueue<T> where T : IComparable
     {
-        private T[] backingArray;
+        private List<T> backingList;
         private int count = 0;
 
         public PriorityQueue()
         {
-            backingArray = new T[count]; ;
+            backingList = new List<T>();
         }
 
         public void Add(T value)
         {
             count++;
-            ResizeIfNeeded();
-            backingArray[count - 1] = value;
+            backingList.Add(value);
             SwitchPlacesIfNeeded();
 
         }
@@ -29,47 +29,29 @@ namespace PriorityQueueLibrary
 
         public T Peek()
         {
-            
-            return backingArray[0];
+            return backingList[0];
         }
 
         public T Pop()
         {
-            T temp = backingArray[0];
-            backingArray[0] = backingArray[count - 1];
-            backingArray[count - 1] = default;
+            T temp = backingList[0];
+            backingList[0] = backingList[count - 1];
+            backingList[count - 1] = default;
             count--;
             SwitchPlacesIfNeeded();
             return temp;
         }
 
-        private void ResizeIfNeeded()
-        {
-            T[] tempArray = null;
-
-            if (count > backingArray.Length)
-            {
-                tempArray = new T[Count() + 1];
-
-                for (int i = 0; i < backingArray.Length; i++)
-                {
-                    tempArray[i] = backingArray[i];
-                }
-
-                backingArray = tempArray;
-            }
-        }
-
         private void SwitchPlacesIfNeeded()
         {
-            for (int i = 0; i < count; i++)
+            for (int i = count - 1; i >= 0; i--)
             {
-                if (backingArray[i].CompareTo(backingArray[(i - 1)/2]) == -1)
+                if (backingList[i].CompareTo(backingList[(i - 1)/2]) == -1)
                 {
                     T temp = default;
-                    temp = backingArray[i];
-                    backingArray[i] = backingArray[(i - 1)/2];
-                    backingArray[(i - 1)/ 2] = temp;
+                    temp = backingList[i];
+                    backingList[i] = backingList[(i - 1)/2];
+                    backingList[(i - 1)/ 2] = temp;
 
                 }
             }
--- PriorityQueue_Tests/UnitTest1.cs
+++ PriorityQueue_Tests/UnitTest1.cs
@@ -16,5 +16,24 @@ namespace Tests
 
             Assert.AreEqual(4, que.Count());
         }
+
+        [Test]
+        public void Test2()
+        {
+            PriorityQueue<int> que = new PriorityQueue<int>();
+            que.Add(9);
+            que.Add(8);
+            que.Add(7);
+            que.Add(6);
+            que.Add(5);
+            que.Add(4);
+            que.Add(3);
+            que.Add(2);
+
+            que.Pop();
+
+            Assert.IsTrue(que.Pop() == 3);
+        }
+
     }
 }
