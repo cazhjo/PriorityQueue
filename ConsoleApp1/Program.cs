c65912de8e9d2ef74c13df4626d524c3a796ebc1
cazhjo <54894732+cazhjo@users.noreply.github.com>
2019-10-08 13:36:47 +0200

    Added method to swap elements. And made BubbleUp and BubbleDown O(log n)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,10 +8,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //PriorityQueue<int> que = new PriorityQueue<int>();
+            PriorityQueue<int> que = new PriorityQueue<int>();
+            que.Add(10);
+            que.Add(9);
+            que.Add(8);
+            que.Add(7);
+            que.Add(6);
+            que.Add(5);
+            que.Add(4);
+            que.Add(3);
+            que.Add(2);
+            que.Add(1);
+
+            que.Pop();
+            que.Pop();
+            que.Pop();
+            que.Pop();
 
 
-            PriorityQueueTester.TestPriorityQueue(() => new PriorityQueue<int>(), () => new PriorityQueue<string>());
+
+            //PriorityQueueTester.TestPriorityQueue(() => new PriorityQueue<int>(), () => new PriorityQueue<string>());
         }
     }
 }
--- PriorityQueueLibrary/PriorityQueue.cs
+++ PriorityQueueLibrary/PriorityQueue.cs
@@ -16,17 +16,9 @@ namespace PriorityQueueLibrary
 
         public void Add(T value)
         {
-            if (!(value.CompareTo(default) == -1))
-            {
-                count++;
-                backingList.Add(value);
-                SwitchPlacesIfNeeded();
-            }
-            else
-            {
-                return;
-            }
-
+            count++;
+            backingList.Add(value);
+            BubbleUp();
         }
 
         public int Count()
@@ -36,42 +28,91 @@ namespace PriorityQueueLibrary
 
         public T Peek()
         {
-            return count != 0 ? backingList[0] : (default);
+            return count != 0 ? backingList[0] : throw new InvalidOperationException();
         }
 
         public T Pop()
         {
             if (count == 0)
             {
-                return default;
+                throw new InvalidOperationException();
             }
             else
             {
-                T temp = default;
-                temp = backingList[0];
+                T temp = backingList[0];
                 backingList[0] = backingList[count - 1];
                 backingList.RemoveAt(count - 1);
                 count--;
-                SwitchPlacesIfNeeded();
+                BubbleDown();
                 return temp;
             }
 
+
+        }
+
+        private void BubbleUp()
+        {
+            int i = count - 1;
+
+            while (backingList[i].CompareTo(backingList[(i - 1) / 2]) == -1) 
+            {
+                SwapTwoElements(i, (i - 1) / 2);
+
+                i = (i - 1) / 2;
+            }
+        }
+
+        private void BubbleDown()
+        {
+
+            if (count == 2)
+            {
+                if(backingList[0].CompareTo(backingList[1]) == 1)
+                {
+                    SwapTwoElements(0, 1);
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else if(count <= 1)
+            {
+                return;
+            }
+
+            int i = 0;
+            while (backingList[i].CompareTo(backingList[2 * i + 1]) == 1 || backingList[i].CompareTo(backingList[2 * i + 2]) == 1)
+            {
+                if(backingList[2 * i + 1].CompareTo(backingList[2 * i + 2]) == -1)
+                {
+                    SwapTwoElements(i, 2 * i + 1);
+
+                    i = 2 * i + 1;
+                }
+                else
+                {
+                    SwapTwoElements(i, 2 * i + 2);
+                    i = 2 * i + 2;
+                }
+
+                if(2 * i + 1 >= count || 2 * i + 2 >= count)
+                {
+                    break;
+                }
+            }
             
         }
 
-        private void SwitchPlacesIfNeeded()
+        private void SwapTwoElements(int index1, int index2)
         {
-            for (int i = count - 1; i >= 0; i--)
-            {
-                if (backingList[i].CompareTo(backingList[(i - 1) / 2]) == -1)
-                {
-                    T temp = default;
-                    temp = backingList[i];
-                    backingList[i] = backingList[(i - 1) / 2];
-                    backingList[(i - 1) / 2] = temp;
-
-                }
-            }
+            T temp = backingList[index1];
+            backingList[index1] = backingList[index2];
+            backingList[index2] = temp;
         }
     }
 }
+
+
+
