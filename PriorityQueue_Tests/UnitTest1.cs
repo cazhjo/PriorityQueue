ec25546c0edaf18add6d723685e213a19222c1fe
cazhjo <54894732+cazhjo@users.noreply.github.com>
2019-10-08 15:20:13 +0200

    Added new unit tests

--- PriorityQueue_Tests/UnitTest1.cs
+++ PriorityQueue_Tests/UnitTest1.cs
@@ -1,26 +1,21 @@
 using NUnit.Framework;
 using PriorityQueueLibrary;
+using System;
 
 namespace Tests
 {
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void TestThatAdd_Works()
         {
             PriorityQueue<int> que = new PriorityQueue<int>();
-            que.Add(1);
-            que.Add(2);
-            que.Add(3);
-            que.Add(4);
-
-            Assert.AreEqual(4, que.Count());
-        }
-
-        [Test]
-        public void Test2()
-        {
-            PriorityQueue<int> que = new PriorityQueue<int>();
+            que.Add(15);
+            que.Add(14);
+            que.Add(13);
+            que.Add(12);
+            que.Add(11);
+            que.Add(10);
             que.Add(9);
             que.Add(8);
             que.Add(7);
@@ -29,16 +24,53 @@ namespace Tests
             que.Add(4);
             que.Add(3);
             que.Add(2);
+            que.Add(1);
 
-            que.Pop();
-            que.Pop();
-            que.Pop();
-            que.Pop();
-            que.Pop();
-            que.Pop();
-
-            Assert.IsTrue(que.Pop() == 8);
+            Assert.AreEqual(1, que.Peek());
         }
 
+        [Test]
+        public void TestThatPop_Works()
+        {
+            PriorityQueue<int> que = new PriorityQueue<int>();
+            que.Add(10);
+            que.Add(9);
+            que.Add(8);
+            que.Add(7);
+            que.Add(6);
+            que.Add(5);
+            que.Add(4);
+            que.Add(3);
+            que.Add(2);
+            que.Add(1);
+
+            que.Pop();
+            que.Pop();
+            que.Pop();
+            que.Pop();
+            que.Pop();
+            que.Pop();
+
+            Assert.AreEqual(7, que.Pop());
+            
+        }
+
+        [Test]
+        public void TestThatPeek_OnEmptyList_ThrowsInvalidOperationException()
+        {
+            PriorityQueue<int> que = new PriorityQueue<int>();
+
+            Assert.Throws<InvalidOperationException>(() => que.Peek());
+            
+        }
+
+        [Test]
+        public void TestThatPop_OnEmptyList_ThrowsInvalidOperationException()
+        {
+            PriorityQueue<int> que = new PriorityQueue<int>();
+
+            Assert.Throws<InvalidOperationException>(() => que.Pop());
+
+        }
     }
 }
